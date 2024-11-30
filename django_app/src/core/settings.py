9fbe1dcf888e1203e77fe3c2fb420d3e6bfa812c
balancy <balancy@gmail.com>
2024-11-30 23:16:53 +0100

    Add some bootstrap styling.

--- django_app/src/core/settings.py
+++ django_app/src/core/settings.py
@@ -90,7 +90,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
